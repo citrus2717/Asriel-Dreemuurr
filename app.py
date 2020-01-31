53723f48bc1e4e2eea756ae5c172e69c0f35ff87
citrus2717 <30320701+citrus2717@users.noreply.github.com>
2020-02-01 00:49:21 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -27,5 +27,22 @@ def handle_message(event):
     message = TextSendMessage(text='Hello, world')
     line_bot_api.reply_message(event.reply_token, message)
     
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
 if __name__ == "__main__":
     app.run()
