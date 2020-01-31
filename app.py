d416c20bdcc1c3fea4e0c28ea90de19527e00ea6
citrus2717 <30320701+citrus2717@users.noreply.github.com>
2020-01-31 23:42:58 +0800

    app.py
    app

---
+++ app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 23:38:52 2020
+
+@author: Citrus
+"""
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text='Hello World')
+    line_bot_api.reply_message(
+            event.reply_token,
+            message)
