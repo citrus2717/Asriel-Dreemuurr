ca0e2c5476f8e6c4bf692664d3405dbe98617275
citrus2717 <30320701+citrus2717@users.noreply.github.com>
2020-01-31 23:56:02 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,5 @@ Created on Fri Jan 31 23:38:52 2020
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='Hello World')
-    line_bot_api.reply_message(
-            event.reply_token,
-            message)
+    message = TextSendMessage(text='Hello, world')
+    line_bot_api.reply_message(event.reply_token, message)
